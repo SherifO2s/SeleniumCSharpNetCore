b79bc791b101431729e398af779d9c8984d36329
SherifO2s <66255874+SherifO2s@users.noreply.github.com>
2022-10-21 04:50:17 +0100

    intial commit
    intial commit

---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Url = "http://executeautomation.com/sign-in";
+            Assert.Pass();
+        }
+    }
+}
